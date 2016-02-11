ac3dceecfc2fa920e09d3beaeb52b2a67dfc3545
William Knowles <will@hollyandwill.com>
2016-02-11 07:35:32 +0000

    update exercise 2.1

--- chapter_2.js
+++ chapter_2.js
@@ -1,11 +1,13 @@
-// Exercise 2.2 Fizzbuzz
-
+// Exercise 2.1 Looping a triangle
 for (var line = "#"; line.length < 8; line += "#")
   console.log(line);
 
+//this was my solution - i used repeat which wasn't yet shown in the book
+for (var count = 1; count <= 7; count ++){
+  console.log( "#".repeat(count));
+}
 
 // Exercise 2.2 Fizzbuzz
-
 for ( i = 1; i <= 100 ; i++){
  if( !(i % 5) && !(i % 3 )){
   console.log ("FizzBuzz");
@@ -18,9 +20,7 @@ for ( i = 1; i <= 100 ; i++){
 }
 }
 
-
 //Exercise 2.3 Chess board
-
 var white = " #";
 var black = "# ";
 var size = 8;
