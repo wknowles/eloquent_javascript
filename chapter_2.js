5492d2f72dba8238ac84c29e5d3248397f8d65f8
William Knowles <will@hollyandwill.com>
2016-02-13 11:58:49 +0000

    add another triangle!

--- chapter_2.js
+++ chapter_2.js
@@ -7,18 +7,34 @@ for (var count = 1; count <= 7; count ++){
   console.log( "#".repeat(count));
 }
 
+// I then modified to create equalateral triangle instead of right angle
+// need to fix the gap on the top
+
+var space = " ";
+var solid = "#";
+var size = 6;
+var counter = 0;
+
+while (counter < size){
+  var row = space.repeat(size - counter);
+    console.log(row + solid.repeat(counter*2) + row);
+counter ++;
+}
+
 // Exercise 2.2 Fizzbuzz
 for ( i = 1; i <= 100 ; i++){
  if( !(i % 5) && !(i % 3 )){
-  console.log ("FizzBuzz");
- } else if (!(i % 5)){
+    console.log ("FizzBuzz");
+  } else if (!(i % 5)){
     console.log ("Buzz");
- } else if (!(i % 3)){
-  console.log ("Fizz");
- } else {
-   console.log (i);
+  } else if (!(i % 3)){
+    console.log ("Fizz");
+  } else {
+    console.log (i);
+  }
 }
-}
+
+//  Exercise 2.2 Fizzbuzz - example using switch statement
 
 //Exercise 2.3 Chess board
 var white = " #";
