16fa05d51af7c72cdfad1d99651e2207419f99d7
William Knowles <will@hollyandwill.com>
2016-02-14 09:20:44 +0000

    Exercise 3.2 Recursion

--- chapter_3.js
+++ chapter_3.js
@@ -1,13 +1,10 @@
 // Exercise 3.1  Minimum
-
 // using Math.min
-
 function min(param1, param2){
   return Math.min(param1, param2);
 }
 
 // or without
-
 function min(a, b){
   if (a < b){
       return a;
@@ -20,3 +17,26 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+
+
+// Exercise 3.2 Recursion
+// Your code here.
+function isEven(n){
+  if (n === 0 )
+      return true;
+  else if (n === 1)
+      return false;
+//for a negative number; check whether n is less than zero and recurse as negative
+  else if (n < 0)
+    return isEven(-n);
+  else
+    return isEven(n-2);
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
