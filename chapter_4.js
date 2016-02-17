366f2f1551bbf5aa16bd23a62d79f5343534f505
William Knowles <will@hollyandwill.com>
2016-02-17 19:53:26 +0000

    Exercise 4.2 Reversing an Array

--- chapter_4.js
+++ chapter_4.js
@@ -18,9 +18,43 @@ function range(start, end, step){
   return rangeNum;
 }
 
-console.log(range(1, 10, 3));
+function sum(range) {
+  var total = 0;
+  for (var i = 0; i < range.length; i++){
+    total += range[i];
+  }
+  return total;
+}
+
+console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -2));
+console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
 // → 55
+
+// Exercise 4.2 Reversing an Array
+function reverseArray(array){
+  var newReverseArray = [];
+  for (i = array.length - 1; i >= 0; i--){
+    newReverseArray.push(array[i]);
+  }
+  return newReverseArray;
+}
+
+function reverseArrayInPlace(array){
+  for (i = 0; i < Math.floor(array.length / 2); i++){
+    var holdVar = array[i];
+    array[i] = (arrayValue[array.length - 1 - i]);
+    (arrayValue[array.length - 1 - i]) = holdVar;
+  }
+  return reverseArrayInPlace;
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+var arrayValue = [1, 2, 3, 4, 5];
+console.log(arrayValue);
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
