6b7d9848545f3167cba640e41e7ebc43f6c4926e
William Knowles <will@hollyandwill.com>
2016-02-18 20:39:31 +0000

    Exercise 4.3 A List

--- chapter_4.js
+++ chapter_4.js
@@ -33,6 +33,8 @@ console.log(range(5, 2, -1));
 console.log(sum(range(1, 10)));
 // → 55
 
+
+
 // Exercise 4.2 Reversing an Array
 function reverseArray(array){
   var newReverseArray = [];
@@ -58,3 +60,46 @@ console.log(arrayValue);
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
+
+
+
+// Exercise 4.3 A List
+function arrayToList(array){
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--) {
+    list = {value: array[i], rest: list};
+  }
+  return list;
+}
+
+function listToArray(list){
+  var array = [];
+  for (var node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+//prepend, which takes an element and a list
+//and creates a new list that adds the element
+//to the front of the input list
+function prepend(value, list) {
+  return {value: value, rest: list};
+}
+
+function nth(list, i){
+  if( i === 0 ){
+    return list.value;
+  } else {
+    return nth(list.rest, i - 1);
+  }
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
