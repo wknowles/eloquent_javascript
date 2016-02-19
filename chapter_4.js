e9e883b1cbdecf99879d069d2fe64b1f428675e2
William Knowles <will@hollyandwill.com>
2016-02-19 07:45:01 +0000

    Exercise 4.4 Deep Comparison
    I used all the help I could get for this one! I should definitely
    retake this chapter.

--- chapter_4.js
+++ chapter_4.js
@@ -103,3 +103,34 @@ console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
+
+// Exercise 4.4 Deep Comparison
+
+function deepEqual(a,b){
+  if(a === b){
+    return true;
+  }
+  if(a === null || typeof a != "object" ||
+     b === null || typeof b != "object"){
+  return false;
+  }
+    var propsInA = 0; var propsInB = 0;
+
+  for (var prop in a)
+    propsInA += 1;
+
+  for (prop in b){
+    propsInB += 1;
+    if (!(prop in a) || !deepEqual(a[prop], b[prop]))
+      return false;
+  }
+  return propsInA === propsInB;
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
